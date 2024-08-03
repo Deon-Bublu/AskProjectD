15770a2672b19b99af865841358d3def4c6a0004
DeonBuble <Osetrov01v1@list.ru>
2024-08-03 22:15:21 +0500

    ls5.5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+
+  before_action :set_question!, only: %i[show destroy edit update]
   def index
     @questions = Question.all
   end
@@ -18,11 +20,9 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find_by id: params[:id]
   end
 
   def update
-    @question = Question.find_by id: params[:id]
     if @question.update question_params
       flash[:success] = "Question updated!"
       redirect_to questions_path
@@ -32,14 +32,14 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by id: params[:id]
     @question.destroy
     flash[:success] = "Question deleted!"
     redirect_to questions_path
   end
 
   def show
-    @question = Question.find_by id: params[:id]
+    @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
   end
 
   private
@@ -48,5 +48,8 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body)
   end
 
+  def set_question!
+    @question = Question.find params[:id]
+  end
 
 end
