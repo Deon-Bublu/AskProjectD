39676e1bc0785b05d5428a08b87fb1eeb97e4f7d
DeonBuble <Osetrov01v1@list.ru>
2024-08-06 12:39:14 +0500

    ls5f

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,10 @@
 class AnswersController < ApplicationController
 
   before_action :set_question!
+  before_action :set_answer!, except: :create
 
   def destroy
-    answer = @question.answers.find params[:id]
-    answer.destroy
+    @answer.destroy
     flash[:success] = "Answer deleted!"
     redirect_to question_path(@question)
   end
@@ -20,6 +20,18 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @answer.update answer_params
+      flash[:success] = "Answer updated!"
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def answer_params
@@ -27,5 +39,10 @@ class AnswersController < ApplicationController
   end
   def set_question!
     @question = Question.find params[:question_id]
+  end
+
+
+  def set_answer!
+    @answer = @question.answers.find params[:id]
   end
 end
