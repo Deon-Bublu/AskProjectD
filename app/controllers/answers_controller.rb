f15b37b991c7711dcccdb78d2238647acf9c3d28
DeonBuble <Osetrov01v1@list.ru>
2024-08-12 00:01:26 +0500

    ls7

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,7 +6,7 @@ class AnswersController < ApplicationController
   def destroy
     @answer.destroy
     flash[:success] = "Answer deleted!"
-    redirect_to question_path(@question)
+    redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
   end
   def create
     @answer = @question.answers.build answer_params
