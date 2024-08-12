e705a3dcacfbea364a6a0dc048de0e9c97a56c51
DeonBuble <Osetrov01v1@list.ru>
2024-08-13 00:14:52 +0500

    ls7.2

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,11 +3,7 @@ class AnswersController < ApplicationController
   before_action :set_question!
   before_action :set_answer!, except: :create
 
-  def destroy
-    @answer.destroy
-    flash[:success] = "Answer deleted!"
-    redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
-  end
+
   def create
     @answer = @question.answers.build answer_params
 
@@ -26,10 +22,16 @@ class AnswersController < ApplicationController
   def update
     if @answer.update answer_params
       flash[:success] = "Answer updated!"
-      redirect_to question_path(@question)
+      redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
     else
       render :edit
     end
+  end
+
+  def destroy
+    @answer.destroy
+    flash[:success] = "Answer deleted!"
+    redirect_to question_path(@question)
   end
 
   private
